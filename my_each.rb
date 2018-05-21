2074cfb1362b5c946e3485270a7ccd996fa6c96e
Dennis Lord <lorddennis1006@gmail.com>
2018-05-21 19:19:07 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,11 @@
-tas = ['arel', 'jon', 'logan', 'spencer']
-def my_each(tas)
-  my_each(tas) do |ta|
-    times_called += 1
+def my_each(collection)
+  length = collection.length
+
+  i = 0
+  while i < length
+    yield(collection[i])
+    i = i + 1
   end
+
+  collection
 end
